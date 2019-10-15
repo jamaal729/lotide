6871d240965f0450a42f6ab5686cf766d080f2aa
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-15 16:55:44 +0000

    removed comparisons based on type

--- assertEqual.js
+++ assertEqual.js
@@ -1,25 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (typeof (actual) !== typeof (expected)) {
-    console.log(`❗️  Error: The actual and expected values are not of the same type`);
+  if (actual === expected) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    switch (typeof (actual) && typeof (expected)) {
-    case "string":
-      if (actual === expected) {
-        console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
-      } else {
-        console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
-      }
-      break;
-    case "number":
-      if (actual === expected) {
-        console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
-      } else {
-        console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
-      }
-      break;
-    default:
-    }
+    console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -28,5 +12,3 @@ assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, 2);
-
-// https://stackoverflow.com/questions/53746913/why-does-console-log-log-strings-with-quotes-in-some-cases
