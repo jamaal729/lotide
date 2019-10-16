1acb7effe57273491994e814a258b824235c69b0
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-16 15:45:47 +0000

    Implemented assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  let equal;
+  if (arr1.length !== arr2.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        equal = false;
+        break;
+      }
+      equal = true;
+    }
+  }
+  return equal;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
