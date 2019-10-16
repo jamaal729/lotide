c9985e6df2485409b75d5e9e5538a2b0fe1512ed
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-16 15:29:04 +0000

    Implemented eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,40 @@
+const eqArrays = function (arr1, arr2) {
+  let equal;
+  if (arr1.length !== arr2.length) {
+    equal = false;
+  }
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        equal = false;
+        break;
+      }
+      equal = true;
+    }
+  }
+  return equal;
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+
+// // TEST CODE
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, 2);
