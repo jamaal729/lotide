343709cad6f6d225c5262762713537de395854d5
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-21 02:56:54 +0000

    Implemented without

---
+++ without.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1, arr2) {
+  let equal;
+  if (arr1.length !== arr2.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        equal = false;
+        break;
+      }
+    }
+    equal = true;
+  }
+  return equal;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const without = function(source, itemsToRemove) {
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        source.splice(i, 1);
+      }
+    }
+  }
+  return source;
+};
+
+console.log("\nLotide - Implement without");
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+assertArraysEqual(without([1, 2, 3], [1]), ["1", "2"]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
